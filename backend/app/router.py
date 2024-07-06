6cabad5ebc6a02334362d20bb48137a4961fc21e
Иван Филатов <fivan999@yandex.ru>
2024-07-06 23:42:19 +0300

    fixed file uploading

--- backend/app/router.py
+++ backend/app/router.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, File
 from backend.app.dependencies import LicensePlateRecognitionDep
+from typing import Annotated
 
 
 router = APIRouter()
 
 
-@router.post('/upload-zip-file')
+@router.post('/api/upload-zip-file')
 async def upload_zip_file(
-    zip_file: UploadFile,
+    zip_file: Annotated[bytes, File()],
     license_plate_recognition: LicensePlateRecognitionDep
 ) -> dict:
     """функция-обработчик архива с картинками"""
-    content = await zip_file.read()
-    recongition_result_bytes = license_plate_recognition.process_zip(content)
+    recongition_result_bytes = license_plate_recognition.process_zip(zip_file)
     return recongition_result_bytes
