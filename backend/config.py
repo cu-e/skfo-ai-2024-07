c711c95c9dd4d6a79be2e88630b530e34328f051
Михаил <58465577+bubled33@users.noreply.github.com>
2024-07-07 09:43:15 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).resolve().parent
 PATH_TO_ZIP_FILES = ROOT_PATH / 'zip_files'
-PATH_TO_RESNET_MODEL = ROOT_PATH / 'models' / 'model_resnet.tflite'
-PATH_TO_NOMER_MODEL = ROOT_PATH / 'models' / 'model1_nomer.tflite'
+PATH_TO_RESNET_MODEL = ROOT_PATH / 'models' / 'model_1.tflite'
+PATH_TO_NOMER_MODEL = ROOT_PATH / 'models' / 'model_2.tflite'
